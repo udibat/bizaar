c16acdd47df28febab2827beab1d7950bf0e2e30
Natalia Shapovalova <natalia.shapovalova@roobykon.com>
2019-04-20 22:28:33 +0300

    #33866

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,31 @@
 $(document).ready(function() {
   $('.home-toolbar').on('click', '.filter-title-btn', function(event) {
     event.preventDefault();
-    var dropdownList = $(this).siblings('.filter-dropdown');
-    dropdownList.toggle();
+    var parent = $(this).parent();
+    var $body = $('body');
+
+    parent.toggleClass('active').siblings().removeClass('active');
+
+    if(parent.hasClass('active')) {
+      $body.addClass('opened');
+    } else {
+      $body.removeClass('opened');
+    }
   });
+
+  $('.home-toolbar').on('click', '.more-options-btn', function(event) {
+    event.preventDefault();
+    var parent = $(this).parent();
+    var resultContainer = $('.search-result-container');
+
+    parent.toggleClass('active');
+
+    if(parent.hasClass('active')) {
+      resultContainer.addClass('opened');
+    } else {
+      resultContainer.removeClass('opened');
+    }
+  });
+
 });
+
