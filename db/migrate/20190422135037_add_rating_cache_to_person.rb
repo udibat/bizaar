0575344dee972f7715d976ff737b001b1672363f
charoyan <kinovhod@gmail.com>
2019-04-23 10:09:50 +0300

    #34019

--- db/migrate/20190422135037_add_rating_cache_to_person.rb
+++ db/migrate/20190422135037_add_rating_cache_to_person.rb
@@ -1,7 +1,7 @@
 class AddRatingCacheToPerson < ActiveRecord::Migration[5.1]
   def up
     blank = { count: 0, avg: 0 }.to_yaml
-    add_column :listings, :rating_cache, :string, default: blank
+    add_column :listings, :rating_cache, :string, default: blank unless column_exists? :listings, :rating_cache
     Listing.find_each do |listing|
       listing.reset_rating_cache!
     end
