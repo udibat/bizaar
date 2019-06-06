e283451d5039021aae87eecc4259c1016369826d
Vladimir Kulakov <bbcoders.v@gmail.com>
2019-06-06 20:11:48 +0300

    #34538 some fixes

--- lib/st_customization/config/routes.rb
+++ lib/st_customization/config/routes.rb
@@ -1,16 +1,16 @@
 StCustomization::Engine.routes.draw do
 
-  get 'tutor_wizard/continue' => 'tutor_wizard#continue', as: :tutor_wizard_continue
-  get 'tutor_wizard/qualifications' => 'tutor_wizard#qualifications', as: :tutor_wizard_qualifications
+  get '/tutor_wizard/continue' => 'tutor_wizard#continue', as: :tutor_wizard_continue
+  get '/tutor_wizard/qualifications' => 'tutor_wizard#qualifications', as: :tutor_wizard_qualifications
 
-  get 'tutor_wizard/email_verification_finished' => 'tutor_wizard#email_verification_finished', as: :tutor_wizard_email_verification_finished
-  get 'tutor_wizard/profile_picture' => 'tutor_wizard#profile_picture', as: :tutor_wizard_profile_picture
-  get 'tutor_wizard/describe_yourself' => 'tutor_wizard#describe_yourself', as: :tutor_wizard_describe_yourself
-  get 'tutor_wizard/cover_photos' => 'tutor_wizard#cover_photos', as: :tutor_wizard_cover_photos
-  get 'tutor_wizard/social_media' => 'tutor_wizard#social_media', as: :tutor_wizard_social_media
-  get 'tutor_wizard/id_verification' => 'tutor_wizard#id_verification', as: :tutor_wizard_id_verification
-  get 'tutor_wizard/payment_information' => 'tutor_wizard#payment_information', as: :tutor_wizard_payment_information
-  get 'tutor_wizard/bizaar_pact' => 'tutor_wizard#bizaar_pact', as: :tutor_wizard_bizaar_pact
+  get '/tutor_wizard/email_verification_finished' => 'tutor_wizard#email_verification_finished', as: :tutor_wizard_email_verification_finished
+  get '/tutor_wizard/profile_picture' => 'tutor_wizard#profile_picture', as: :tutor_wizard_profile_picture
+  get '/tutor_wizard/describe_yourself' => 'tutor_wizard#describe_yourself', as: :tutor_wizard_describe_yourself
+  get '/tutor_wizard/cover_photos' => 'tutor_wizard#cover_photos', as: :tutor_wizard_cover_photos
+  get '/tutor_wizard/social_media' => 'tutor_wizard#social_media', as: :tutor_wizard_social_media
+  get '/tutor_wizard/id_verification' => 'tutor_wizard#id_verification', as: :tutor_wizard_id_verification
+  get '/tutor_wizard/payment_information' => 'tutor_wizard#payment_information', as: :tutor_wizard_payment_information
+  get '/tutor_wizard/bizaar_pact' => 'tutor_wizard#bizaar_pact', as: :tutor_wizard_bizaar_pact
 
 
 end
