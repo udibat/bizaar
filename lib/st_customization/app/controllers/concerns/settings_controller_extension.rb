259493334bf539b84782a5cbce63306a758d9c49
Vladimir Kulakov <bbcoders.v@gmail.com>
2019-07-05 14:33:40 +0300

    #35153 add member settings pages

--- lib/st_customization/app/controllers/concerns/settings_controller_extension.rb
+++ lib/st_customization/app/controllers/concerns/settings_controller_extension.rb
@@ -3,6 +3,7 @@ require 'active_support/concern'
 module SettingsControllerExtension
   extend ActiveSupport::Concern
   include TutorChecker
+  include MemberChecker
 
   def qualifications
     @existing_certifications = @current_user.custom_profile.certifications
@@ -12,10 +13,15 @@ module SettingsControllerExtension
     @existing_id_verifications = @current_user.custom_profile.id_verifications
   end
 
+  def member_payments
+    @selected_left_navi_link = "Payments"
+  end
 
   included do
 
-    before_action :ensure_user_is_tutor, only: [:qualifications, :id_verification]
+    before_action :ensure_user_is_tutor, only: [:qualifications]
+    before_action :ensure_user_is_member, only: [:member_payments]
+    before_action :manage_view_path
 
   end
 
@@ -23,7 +29,13 @@ module SettingsControllerExtension
 
   # end
 
-# private
+private
+
+  def manage_view_path
+    unless @current_user.is_tutor?
+      prepend_view_path Rails.root.to_s + "/lib/st_customization/app/views/member_settings"
+    end
+  end
   
 
 end
