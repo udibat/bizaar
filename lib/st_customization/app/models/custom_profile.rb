cd39b3b649402dca0e67b3a037abccb8415f5543
Vladimir Kulakov <bbcoders.v@gmail.com>
2019-07-10 20:36:09 +0300

    #34901 new image styles and categorized testimonials

--- lib/st_customization/app/models/custom_profile.rb
+++ lib/st_customization/app/models/custom_profile.rb
@@ -2,7 +2,7 @@ class CustomProfile < ApplicationRecord
 
   MAX_DESCRIPTION_LENGTH = 500
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :avatar, styles: { medium: "265x310^#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   belongs_to :person, foreign_key: "person_id"
