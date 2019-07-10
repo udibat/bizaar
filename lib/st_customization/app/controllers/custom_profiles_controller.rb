2cedac07838c3c78f836e13bbbe3f4f0758c3204
Vladimir Kulakov <bbcoders.v@gmail.com>
2019-07-10 15:13:05 +0300

    #35152 fix cover photos removing

--- lib/st_customization/app/controllers/custom_profiles_controller.rb
+++ lib/st_customization/app/controllers/custom_profiles_controller.rb
@@ -81,14 +81,18 @@ private
       if params[:custom_profile][:cover_photos_attributes]['0']['image'].is_a? Array
         images_arr = params[:custom_profile][:cover_photos_attributes]['0']['image']
         params[:custom_profile][:cover_photos_attributes]['0']['image'] = images_arr.shift
-        images_num = params[:custom_profile][:cover_photos_attributes].keys.length
-        images_arr.each_with_index{|im, i|
-           new_i = images_num + i
-           image_blank = params[:custom_profile][:cover_photos_attributes]['0'].clone
-           image_blank['is_primary_photo'] = false
-           image_blank['image'] = im
-           params[:custom_profile][:cover_photos_attributes][new_i.to_s] = image_blank
-        }
+        if params[:custom_profile][:cover_photos_attributes]['0']['image'].blank?
+          params[:custom_profile][:cover_photos_attributes].delete('0')
+        else
+          images_num = params[:custom_profile][:cover_photos_attributes].keys.length
+          images_arr.each_with_index{|im, i|
+             new_i = images_num + i
+             image_blank = params[:custom_profile][:cover_photos_attributes]['0'].clone
+             image_blank['is_primary_photo'] = false
+             image_blank['image'] = im
+             params[:custom_profile][:cover_photos_attributes][new_i.to_s] = image_blank
+          }
+        end
       end
       params[:custom_profile][:cover_photos_attributes].keys.each{|k|
         params[:custom_profile][:cover_photos_attributes][k.to_s].permit(:id, :_destroy, :image, :is_primary_photo)
