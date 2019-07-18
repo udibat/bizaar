c4e4b942fd065c84a5a8bbf7b69aca0d9d39ea6b
Vladimir Kulakov <bbcoders.v@gmail.com>
2019-07-18 16:33:12 +0300

    #34935 specify asset host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -105,6 +105,7 @@ Rails.application.configure do
   mail_delivery_method = (APP_CONFIG.mail_delivery_method.present? ? APP_CONFIG.mail_delivery_method.to_sym : :sendmail)
 
   config.action_mailer.delivery_method = mail_delivery_method
+  config.action_mailer.asset_host = APP_CONFIG.asset_host
 
   if mail_delivery_method == :smtp
     ActionMailer::Base.smtp_settings = {
